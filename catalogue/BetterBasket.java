e29da121c90cfdadaf12e7376626343e663fc8eb
charbug <charlabuwu@gmail.com>
2025-01-06 12:28:17 +0000

    Make products stack on BetterBasket.java, plus javadocs

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -5,16 +5,28 @@ import java.util.Collections;
 
 /**
  * Write a description of class BetterBasket here.
- * 
- * @author  Your Name 
+ *
+ * @author Your Name
  * @version 1.0
  */
-public class BetterBasket extends Basket implements Serializable
-{
-  private static final long serialVersionUID = 1L;
-  
+public class BetterBasket extends Basket implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-  // You need to add code here
-  // merge the items for same product,
-  // or sort the item based on the product number
+    @Override
+    public boolean add(Product pr) {
+        boolean newProductFlag = true;
+
+        for (Object i: super.toArray()) {
+            Product current = (Product) i;
+            if (current.getProductNum().equals(pr.getProductNum())) {
+                newProductFlag = false;
+                current.setQuantity(current.getQuantity() + 1);
+            }
+        }
+        if (newProductFlag) {return super.add(pr);} else {return false;}
+    }
+
+    // You need to add code here
+    // merge the items for same product,
+    // or sort the item based on the product number
 }
